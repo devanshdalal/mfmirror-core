d0be80ec3581cb8b39108daf95142c7380a11d48
Devansh <mr.devanshdalal@gmail.com>
2019-07-17 19:09:29 +0530

    fix manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mfmirror_server.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mfmirror_server')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
