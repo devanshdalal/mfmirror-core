03dfc556b0fd7e8318a9bbfbb20895e07442f27a
Devansh <mr.devanshdalal@gmail.com>
2019-07-17 19:24:44 +0530

    fix for DJANGO_SETTINGS_MODULE

--- mfmirror_server/wsgi.py
+++ mfmirror_server/wsgi.py
@@ -10,7 +10,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 # from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mfmirror_server')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mfmirror_server.settings')
 
 application = get_wsgi_application()
 # application = DjangoWhiteNoise(application)
