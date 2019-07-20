4302fdc5588550b5f52897813b09a35b8ddf032f
Devansh <mr.devanshdalal@gmail.com>
2019-07-20 14:40:44 +0530

    Fix getportfolio, changed to POST from GET

--- coreapp/views.py
+++ coreapp/views.py
@@ -1,17 +1,18 @@
 import os
-
-from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from subprocess import call
 
 from django.conf import settings
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
 
 from util.analyzer import WeightedAverage
 from util.utils import BASE_DIR_CSVS
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
-from subprocess import call
 
 # Installing scheduler
 
@@ -34,12 +35,13 @@ class GetFundsView(TemplateView):
         global scheduler
         if scheduler == None:
             scheduler = BackgroundScheduler()
-            scheduler.add_job(scrpper_job, 'interval', seconds=600)
+            scheduler.add_job(scrpper_job, 'interval', seconds=60)
             scheduler.start()
         funds = list(map(lambda x: os.path.splitext(x)[0], filter(lambda y: not y.startswith('.'), os.listdir(BASE_DIR_CSVS))))
         return JsonResponse(funds, safe=False)
 
+@method_decorator(csrf_exempt, name='dispatch')
 class GetPortfolioView(TemplateView):
-    def get(self, request, **kwargs):
+    def post(self, request, **kwargs):
         funds = WeightedAverage(request.GET.dict(), to_json=True)
         return HttpResponse(funds, content_type='application/json')
